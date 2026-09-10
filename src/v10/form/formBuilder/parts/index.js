b8998b47c468a2ce763f428442590beabbaf29cf
keshavsoft <nknnkumar@yahoo.com>
2026-09-10 14:07:41 +0530

    create 1

--- src/v10/form/formBuilder/parts/index.js
+++ src/v10/form/formBuilder/parts/index.js
@@ -2,7 +2,7 @@ import { buildContainer } from "./container/index.js";
 import { buildHead } from "./head/index.js";
 import { buildBody } from "./body/index.js";
 import { buildFoot } from "./foot/index.js";
-import buildBodyV1 from "./buildBody/v1/index.js";
+import buildBodyV1 from "./buildBody/v2/index.js";
 
 export { buildContainer, buildHead, buildBody, buildFoot };
 export { buildBodyV1 };
